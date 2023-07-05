12988e878c8d1aa078353ddb37b5177dda483f95
Luis Herrera <luis378962@gmail.com>
2023-07-05 15:08:17 -0600

    Semaforo function

--- src/Semaforo.jsx
+++ src/Semaforo.jsx
@@ -1,24 +1,25 @@
 import React from 'react'
-
-const [active, setActive ] = useState('green')
-
-
-useEffect(() => {
-  const intervalo = setInterval((prevFoco) => {
-    setActive(() => {
-      switch(prevFoco) {
-        case 'green': 
-          return 'green';
-        case 'yellow':
-          return 'yellow';
-        default:
-          return 'red'; 
-      }
-    },)
-  }, 2000)
-}, [])
+import { useState, useEffect } from 'react'
 
 const Semaforo = () => {
+g
+  const [active, setActive ] = useState('green')
+
+  useEffect(() => {
+    const intervalo = setInterval((prevFoco) => {
+      setActive(() => {
+        switch(prevFoco) {
+          case 'green': 
+            return 'green';
+          case 'yellow':
+            return 'yellow';
+          default:
+            return 'red'; 
+        }
+      },)
+    }, 2000)
+  }, [])
+
   return (
     <div> Semaforo </div>
   )
